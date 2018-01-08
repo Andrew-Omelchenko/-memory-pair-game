2e2ef2d493d30d98876b992fb52682c556b9b81b
Andrew Omelchenko <andrewom43@gmail.com>
2018-01-08 19:41:56 +0200

    Add event listener for cells

--- js/code.js
+++ js/code.js
@@ -27,6 +27,7 @@ Cell.prototype.draw = function() {
 	front_div.style.backgroundColor = this.face_color;
 	var back_div = document.createElement("div");
 	back_div.classList.add("back");
+	back_div.style.color = this.face_color;	// trick only
 	var sub_root_div = document.createElement("div");
 	sub_root_div.classList.add("flipper");
 	sub_root_div.appendChild(front_div);
@@ -62,6 +63,7 @@ var Game = function(table) {
 	this.gameTable = new GameTable(table);
 	this.selectedCells = [];
 	this.nTurns = 0;
+	this.nPairsFound = 0;
 };
 Game.prototype.init = function() {
 	this.gameTable.shuffle();
@@ -77,7 +79,54 @@ window.onload = function() {
 	// TODO
 	var game = new Game(colorsTable);
 	game.init();
+
+	// add event listener to Reset button
 	document.getElementById("reset_button").addEventListener("click", function() {
 		window.location.reload();
 	});
+
+	// add event listener to cells
+	document.addEventListener("click", function(event) {
+		var isBack = (event.target.className == "back");
+		var isHidden = (event.target.parentElement.className == "hidden-cell");
+		var isSelected = (event.target.parentElement.className == "selected-cell")
+		if (isBack && !(isSelected || isHidden)) {
+			turnOver(event.target);
+			if (game.selectedCells.length == 0) {
+				game.selectedCells.push(event.target);
+			} else if (game.selectedCells.length == 1) {
+				var oldEventTarget = game.selectedCells[0];
+				if (event.target.style.color == oldEventTarget.style.color) {
+					setTimeout(function() {
+						hide(event.target);
+						hide(oldEventTarget);
+					}, 1200);
+					game.nPairsFound++;
+				} else {
+					setTimeout(function() {
+						turnOver(event.target);
+						turnOver(oldEventTarget);
+					}, 1200);
+				}
+				game.nTurns++;
+				game.updateCounter();
+				game.selectedCells = [];
+			}
+		}
+		if (game.nPairsFound == 8) {
+			setTimeout(function() {
+				alert("Congratulations!!! You won in " + game.nTurns + " turns.");
+				window.location.reload();
+			}, 1400);
+		}
+	});
+};
+
+// utility functions
+function turnOver(targetCell) {
+	targetCell.parentElement.classList.toggle("selected-cell");
+};
+
+function hide(targetCell) {
+	targetCell.parentElement.classList.toggle("hidden-cell");
 };
