ca72f1bb9e55dbade36cd8db4aeac990eacb271e
Andrew Omelchenko <andrewom43@gmail.com>
2018-01-08 12:12:29 +0200

    Add classes Cell and GameTable

--- js/code.js
+++ js/code.js
@@ -1,4 +1,66 @@
-function Shuffle(o) {
-	for(var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-	return o;
+var colorsTable = [
+	"Red",
+	"Green",
+	"Blue",
+	"Cyan",
+	"Magenta",
+	"Yellow",
+	"Brown",
+	"GreenYellow ",
+	"Red",
+	"Green",
+	"Blue",
+	"Cyan",
+	"Magenta",
+	"Yellow",
+	"Brown",
+	"GreenYellow "
+];
+
+// class Cell
+var Cell = function(color) {
+	this.face_color = color;
+};
+Cell.prototype.draw = function() {
+	var front_div = document.createElement("div");
+	front_div.classList.add("front");
+	front_div.style.backgroundColor = this.face_color;
+	var back_div = document.createElement("div");
+	back_div.classList.add("back");
+	var sub_root_div = document.createElement("div");
+	sub_root_div.classList.add("flipper");
+	sub_root_div.appendChild(front_div);
+	sub_root_div.appendChild(back_div);
+	var root_div = document.createElement("div");
+	root_div.classList.add("flip-container");
+	root_div.appendChild(sub_root_div);
+	var table = document.getElementById("game-table");
+	table.appendChild(root_div);
+};
+
+// class GameTable
+var GameTable = function(table) {
+	this.table = table;
+};
+GameTable.prototype.shuffle = function() {
+	for(var j, x, i = this.table.length; 
+		i; 
+		j = parseInt(Math.random() * i), 
+		x = this.table[--i], 
+		this.table[i] = this.table[j], 
+		this.table[j] = x);
+};
+GameTable.prototype.draw = function() {
+	for(var index = 0; index < this.table.length; index++) {
+		var cell = new Cell(this.table[index]);
+		cell.draw();
+	}
+};
+
+window.onload = function() {
+	// TODO
+	var gameTable = new GameTable(colorsTable);
+	gameTable.shuffle();
+	gameTable.draw();
+
 };
